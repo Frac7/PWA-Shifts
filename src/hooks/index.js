a734bf8432230c4d8169a035d869ee99d0d51cde
francescacella <francesca.cella@spindox.it>
2022-02-20 17:11:02 +0100

    Change useNotification hook logic

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -7,44 +7,46 @@ const useNotifications = () => {
   useEffect(() => {
     if (Notification.permission === 'default') {
       // 1. Request permission for notification for the first time
-      Notification.requestPermission().then((permission) => {
-        if (permission === 'granted') {
-          // 2. When the service worker is ready and notifications are allowed, handle subscription
-          navigator.serviceWorker.ready.then((registration) => {
-            return (
-              registration.pushManager
-                .getSubscription()
-                .then((subscription) => {
-                  // 2a - Return the subscription, if it exists
-                  if (subscription) {
-                    return subscription;
-                  }
-                  // 2b - Otherwise register a subscription to the server using a valid public key provided from the server
-                  fetch(`${process.env.REACT_APP_BE_URL}public-key`)
-                    .then((res) => res.json())
-                    .then((res) => {
-                      const publicKey = urlBase64ToUint8Array(res.publicKey);
-                      return registration.pushManager.subscribe({
-                        userVisibleOnly: true,
-                        applicationServerKey: publicKey,
-                      });
-                    });
-                })
-                // 3 - Send subscription
-                .then((subscription) => {
-                  fetch(`${process.env.REACT_APP_BE_URL}subscription`, {
-                    method: 'POST',
-                    headers: {
-                      'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({
-                      subscription,
-                    }),
+      Notification.requestPermission();
+    }
+  }, []);
+
+  useEffect(() => {
+    if (Notification.permission === 'granted') {
+      // 2. When the service worker is ready and notifications are allowed, handle subscription
+      navigator.serviceWorker.ready.then((registration) => {
+        return (
+          registration.pushManager
+            .getSubscription()
+            .then((subscription) => {
+              // 2a - Return the subscription, if it exists
+              if (subscription) {
+                return subscription;
+              }
+              // 2b - Otherwise register a subscription to the server using a valid public key provided from the server
+              fetch(`${process.env.REACT_APP_BE_URL}public-key`)
+                .then((res) => res.json())
+                .then((res) => {
+                  const publicKey = urlBase64ToUint8Array(res.publicKey);
+                  return registration.pushManager.subscribe({
+                    userVisibleOnly: true,
+                    applicationServerKey: publicKey,
                   });
-                })
-            );
-          });
-        }
+                });
+            })
+            // 3 - Send subscription
+            .then((subscription) => {
+              fetch(`${process.env.REACT_APP_BE_URL}subscription`, {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                  subscription,
+                }),
+              });
+            })
+        );
       });
     }
   }, []);
